c3eee033c95beac65bb12b3bc0bcc622be1391f4
bj <1814291300@qq.com>
2019-07-22 00:47:48 +0800

    内部类

---
+++ src/main/java/com/rigel/basic/base/InnerClass/MethodInnerClassDemo.java
@@ -0,0 +1,37 @@
+package com.rigel.basic.base.InnerClass;
+
+//方法内部类
+//外部类
+public class MethodInnerClassDemo {
+
+    static int b = 1;
+    int a = 0;
+
+    public static void main(String[] args) {
+        MethodInnerClassDemo demo = new MethodInnerClassDemo();
+        demo.show();
+
+    }
+
+    public void show() {
+        final int a = 25;
+        int b = 13;
+
+//        由于方法内部类不能在外部类的方法以外的地方使用，因此方法内部类不能使用访问控制符和 static 修饰符。
+//        static int d = 13;
+//        private int e = 13;
+//        public int f = 13;
+
+        class MInner {
+            int c = 2;
+
+            public void print() {
+                System.out.println("访问外部类的方法中的常量a:" + a);
+                System.out.println("访问内部类中的变量c:" + c);
+                System.out.println(b);
+            }
+        }
+        MInner mi = new MInner();
+        mi.print();
+    }
+}
