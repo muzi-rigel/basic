feef84cc73b1a396ffe9bcc0daad2d9d32d034d7
bj <1814291300@qq.com>
2019-07-23 00:38:31 +0800

    异常链

---
+++ src/main/java/com/rigel/basic/base/Exception/ChainTest2.java
@@ -0,0 +1,22 @@
+package com.rigel.basic.base.Exception;
+
+public class ChainTest2 {
+
+    public static void main(String[] args) {
+        ChainTest2 ct = new ChainTest2();
+        ct.test2();
+    }
+
+    public void test2(){
+        try {
+            test1();
+        } catch (DrunkException e) {
+            RuntimeException newExc = new RuntimeException(e);
+            throw newExc;
+        }
+    }
+
+    public void test1() throws DrunkException{
+        throw new DrunkException("喝酒别开车！");
+    }
+}
