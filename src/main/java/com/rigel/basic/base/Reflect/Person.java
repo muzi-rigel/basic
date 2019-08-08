1c9c9964e379ecef4e7e1a6390b1f2ebad5475a9
bj <1814291300@qq.com>
2019-08-08 15:35:28 +0800

    通过反射获得对象构造方法

--- src/main/java/com/rigel/basic/base/Reflect/Person.java
+++ src/main/java/com/rigel/basic/base/Reflect/Person.java
@@ -1,7 +1,19 @@
 package com.rigel.basic.base.Reflect;
 
+
 public class Person {
 
+    private String name;
+    private int age;
+
+    public Person() {
+
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 
     public void run(){
         System.out.println("人在跑");
