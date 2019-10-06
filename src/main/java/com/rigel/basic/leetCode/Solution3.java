f6be14fa5e88c551431d59b33a87be086a436a24
muzi <362005317@qq.com>
2019-10-06 18:14:25 +0800

    无重复字符的最长子串

---
+++ src/main/java/com/rigel/basic/leetCode/Solution3.java
@@ -0,0 +1,47 @@
+package com.rigel.basic.leetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution3 {
+
+    //超出时间限制
+    public static int lengthOfLongestSubstring1(String s) {
+        int maxLength = s.length() == 1 ? 1 : 0;
+        for (int i = 0; i <= s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                String subString = s.substring(i, j);
+//                System.out.println("sub---" + subString);
+                if (checkDup(subString)) {
+                    maxLength = Math.max(maxLength, j - i);
+//                    System.out.println("ok---" + subString);
+                }
+//                System.out.println("i="+i+" j="+j);
+            }
+        }
+        return maxLength;
+    }
+
+
+    //1  字符串是否重复
+    public static boolean checkDup(String str) {
+        Set<Character> set = new HashSet<Character>();
+        char[] chars = str.toCharArray();
+        for (char aChar : chars) {
+            if (set.contains(aChar)) {
+                return false;
+            }
+            set.add(aChar);
+        }
+        return true;
+    }
+
+    //2
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring1("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring1("bbbbbbbbbb"));
+        System.out.println(lengthOfLongestSubstring1("pwwkew"));
+        System.out.println(lengthOfLongestSubstring1("au"));
+    }
+}
