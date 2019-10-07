fe4a8cd637adafe7e03f6e2f8a854db399405d54
muzi <362005317@qq.com>
2019-10-07 17:42:28 +0800

    修改临界条件

--- src/main/java/com/rigel/basic/leetCode/Solution3.java
+++ src/main/java/com/rigel/basic/leetCode/Solution3.java
@@ -5,13 +5,13 @@ import java.util.Set;
 
 public class Solution3 {
 
-    //超出时间限制
+    //超出时间限制 TLE
     public static int lengthOfLongestSubstring1(String s) {
         int maxLength = s.length() == 1 ? 1 : 0;
-        for (int i = 0; i <= s.length(); i++) {
-            for (int j = i + 1; j <= s.length(); j++) {
-                String subString = s.substring(i, j);
-//                System.out.println("sub---" + subString);
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                String subString = s.substring(i, j + 1);
+                System.out.println("i" + i + "j" + j + "sub---" + subString);
                 if (checkDup(subString)) {
                     maxLength = Math.max(maxLength, j - i);
 //                    System.out.println("ok---" + subString);
@@ -28,9 +28,7 @@ public class Solution3 {
         Set<Character> set = new HashSet<Character>();
         char[] chars = str.toCharArray();
         for (char aChar : chars) {
-            if (set.contains(aChar)) {
-                return false;
-            }
+            if (set.contains(aChar)) return false;
             set.add(aChar);
         }
         return true;
@@ -40,8 +38,10 @@ public class Solution3 {
 
     public static void main(String[] args) {
         System.out.println(lengthOfLongestSubstring1("abcabcbb"));
-        System.out.println(lengthOfLongestSubstring1("bbbbbbbbbb"));
-        System.out.println(lengthOfLongestSubstring1("pwwkew"));
-        System.out.println(lengthOfLongestSubstring1("au"));
+        System.out.println(lengthOfLongestSubstring1("a"));
+        System.out.println(lengthOfLongestSubstring1(""));
+//        System.out.println(lengthOfLongestSubstring1("bbbbbbbbbb"));
+//        System.out.println(lengthOfLongestSubstring1("pwwkew"));
+//        System.out.println(lengthOfLongestSubstring1("au"));
     }
 }
