fc0ae2bc55c138812e4c58192b1f51ed22634ac1
muzi <362005317@qq.com>
2019-11-07 02:30:36 +0800

    jvm 模型

---
+++ src/main/java/com/rigel/basic/base/jvm/model/ByteCodeSample.java
@@ -0,0 +1,9 @@
+package com.rigel.basic.base.jvm.model;
+
+public class ByteCodeSample {
+    public static int add(int a, int b) {
+        int c = 0;
+        c = a + b;
+        return c;
+    }
+}
