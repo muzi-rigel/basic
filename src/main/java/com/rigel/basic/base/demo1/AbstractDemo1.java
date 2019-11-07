33d214426b2a7baea19dc79c42556bee19d5d700
muzi <362005317@qq.com>
2019-11-07 16:56:55 +0800

    抽象类提交

--- src/main/java/com/rigel/basic/base/demo1/AbstractDemo1.java
+++ src/main/java/com/rigel/basic/base/demo1/AbstractDemo1.java
@@ -5,5 +5,7 @@ public class AbstractDemo1 {
     //抽象类和接口的区别
     //抽象类是从多个具体类中抽象出来的父类 作为子类的模板
 
+//    error 抽象方法只能在抽象类中
+//    public abstract void eat();
 
 }
---
+++ src/main/java/com/rigel/basic/base/demo1/AbstractDemo2.java
@@ -0,0 +1,11 @@
+package com.rigel.basic.base.demo1;
+
+public abstract class AbstractDemo2 {
+
+    //抽象类不一定有抽象方法
+    public void eat(){
+
+    }
+
+
+}
