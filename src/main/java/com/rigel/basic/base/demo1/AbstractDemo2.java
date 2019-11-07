9650f64e3546e74e1084cbfac8b7fe1cb8321095
muzi <362005317@qq.com>
2019-11-07 17:34:24 +0800

    抽象类提交

--- src/main/java/com/rigel/basic/base/demo1/AbstractDemo2.java
+++ src/main/java/com/rigel/basic/base/demo1/AbstractDemo2.java
@@ -7,5 +7,21 @@ public abstract class AbstractDemo2 {
 
     }
 
+    //抽象方法不能有方法体
+    public abstract void eat(String foodName);
+
 
 }
+
+class Demo3 extends AbstractDemo2{
+
+    /**
+     * 非抽象子类必须给出抽象类中抽象方法的具体实现
+     * @param foodName
+     */
+    @Override
+    public void eat(String foodName) {
+
+    }
+}
+
