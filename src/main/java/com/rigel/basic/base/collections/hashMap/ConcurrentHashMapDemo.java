96d9632e4cd2441bff70cc872f1454676e1069af
muzi <362005317@qq.com>
2019-12-05 21:49:30 +0800

    12.5 commit

---
+++ src/main/java/com/rigel/basic/base/collections/hashMap/ConcurrentHashMapDemo.java
@@ -0,0 +1,15 @@
+package com.rigel.basic.base.collections.hashMap;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapDemo {
+
+    public static void main(String[] args) {
+
+        ConcurrentHashMap<Integer,Integer> map = new ConcurrentHashMap<>();
+        map.put(1,1);
+        map.put(2,2);
+        map.put(17,17);
+
+    }
+}
--- src/main/java/com/rigel/basic/base/collections/hashMap/GenerCode.java
+++ src/main/java/com/rigel/basic/base/collections/hashMap/GenerCode.java
@@ -4,6 +4,17 @@ public class GenerCode {
 
     public static void main(String[] args) {
 
+        getMap2();
+    }
+
+    private static void getMap2() {
+        for (int i1 = 0; i1 < 13; i1++) {
+            int num = 15*i1;
+            System.out.println("map.put("+num+","+num+");");
+        }
+    }
+
+    private static void getMap() {
         for (int i = 0; i < 10; i++) {
             StringBuilder sb = new StringBuilder();
             for (int i1 = 0; i1 < 7; i1++) {
