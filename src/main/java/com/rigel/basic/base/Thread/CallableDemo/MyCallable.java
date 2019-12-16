fddbcd5019b9b4212161e53029fa398263c45cb7
muzi <362005317@qq.com>
2019-12-16 15:43:11 +0800

    futureTask and Callable demo

---
+++ src/main/java/com/rigel/basic/base/Thread/CallableDemo/MyCallable.java
@@ -0,0 +1,22 @@
+package com.rigel.basic.base.Thread.CallableDemo;
+
+import java.util.concurrent.Callable;
+
+public class MyCallable implements Callable<String> {
+
+    @Override
+    public String call(){
+        String value = "";
+
+        System.out.println("task start...");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        value = "ready to wait!";
+        System.out.println("task down....");
+
+        return value;
+    }
+}
