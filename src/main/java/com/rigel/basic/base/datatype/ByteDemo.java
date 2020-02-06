54de475c672f3e46cfab40f94dbab0c50a345887
muzi <362005317@qq.com>
2020-02-06 15:22:52 +0800

    6日 commit

---
+++ src/main/java/com/rigel/basic/base/datatype/ByteDemo.java
@@ -0,0 +1,35 @@
+package com.rigel.basic.base.datatype;
+
+public class ByteDemo {
+
+    public static void main(String[] args) {
+//        byte b = new Byte();
+
+        //127
+        //-128
+        //32767
+        //-32768
+        //2147483647
+        //-2147483648
+        //9223372036854775807
+        //-9223372036854775808
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+
+        System.out.println(Integer.toBinaryString(1));
+        //11111111111111111111111111111111 反码
+        System.out.println(Integer.toBinaryString(-1));
+
+
+    }
+}
