f5e6741e67e048eb35e94cecf9d1d8709b3c14b2
muzi <362005317@qq.com>
2020-11-03 02:11:25 +0800

    提交之前的内容

---
+++ src/main/java/com/rigel/basic/base/juc/aqs/MLock.java
@@ -0,0 +1,39 @@
+package com.rigel.basic.base.juc.aqs;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+public class MLock implements Lock {
+    private Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
