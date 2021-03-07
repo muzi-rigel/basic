455d653fcb1ae012015998a99d2c08004a58c559
muzi <362005317@qq.com>
2021-03-07 23:53:54 +0800

    7日提交

---
+++ src/main/java/com/rigel/basic/base/datatype/OperationDemo.java
@@ -0,0 +1,12 @@
+package com.rigel.basic.base.datatype;
+
+public class OperationDemo {
+
+    public static void main(String[] args) {
+        char c = 'a';
+        short s = 1;
+        int i = c * s;
+        System.out.println(i);
+
+    }
+}
